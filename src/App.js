0b9597718e72b30d1f80eb04b4076f2f34087b2c
= <=>
2024-10-11 09:47:06 -0300

    revisão do primeiro bimestre

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Data from "./meusComponentes/Data";
-import Contador from "./meusComponentes/ContadorV2";
+import Data from "./meusComponentes/Data"
+import DataV2 from "./meusComponentes/DataV2"
+import Contador from "./meusComponentes/ContadorV2"
 function App() {
   return (
     <div className="App">
-        <Data texto="Data e hora atuais: " timeZone={-3.00}/>
-        <Data texto="Data e hora em Londres: " timeZone={1.00}/>
-        <Data texto="Data e hora em Nova York:" timeZone={-4.00}/>
+        <Data texto="Data e hora atuais: " timeZone = {-3.00}/>
+        <Data texto="Data e hora em Londres: " timeZone = {1.00}/>
+        <Data texto="Data e hora em Nova York:" timeZone = {-4.00}/>
+        <DataV2 texto="Data JSX" timeZone = {-3.00}/>
         <Contador/>
     </div>
   );
